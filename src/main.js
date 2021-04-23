98b5bbae015051ba9ba952772a202cd81ebff071
chenhu2015 <chenhu2015@users.noreply.github.com>
2021-04-23 15:24:59 -0500

    inital project

--- src/main.js
+++ src/main.js
@@ -1,8 +1,30 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import { firestorePlugin } from 'vuefire' // Added to make vuefire work
+import {auth} from './firebaseConfig.js' // for authentication
 
 Vue.config.productionTip = false
+Vue.use(firestorePlugin); // added to make vuefire work
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+let app // for authentication
+auth.onAuthStateChanged(() => { // for authentication
+  if (!app) { // for authentication
+    app = new Vue({
+      router,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
+
+
+// import Vue from 'vue'
+// import App from './App.vue'
+// import router from './router'
+
+// Vue.config.productionTip = false
+
+// new Vue({
+//   router,
+//   render: h => h(App)
+// }).$mount('#app')
