4a3c87290b60c24d92d9151218d799173d6962cb
chenhu2015 <chenhu2015@users.noreply.github.com>
2021-04-25 09:57:48 -0500

    at least three pages work

---
+++ src/components/wishlist-item.vue
@@ -0,0 +1,154 @@
+<template>
+  <div class="wishlist-item" @click="toRestaurant(data)">
+    <div class="top">
+      <img :src="data.pic_url">
+      <!-- v-lazy -->
+      <div class="name">{{data.name}}</div>
+      <!-- <div class="status">5 orders complete</div> -->
+    </div>
+
+    <!-- <div class="mid">
+      <div class="food">Average price: {{data.average_price_tip}}<span></span></div>
+      <div class="food">Average deliverry time: {{data.avg_delivery_time}}<span></span></div>
+      <div class="food">  </div>
+      <div class="cost"><span></span></div>
+    </div> -->
+
+    <div class="bottom">
+      <span class="again">Remove</span>
+      <span class="again">Comment</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data () {
+    return {}
+  },
+  props: {
+    data: {
+      type: Object,
+      default () {
+        return {}
+      }
+    }
+  },
+  watch: {},
+  methods: {
+       toRestaurant (data) {
+      // console.log(data)
+      this.$emit('toRestaurant', data)
+    }
+
+  },
+  filters: {},
+  computed: {},
+  created () {},
+  mounted () {}
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~@/assets/scss/const.scss';
+@import '~@/assets/scss/mixin.scss';
+
+.wishlist-item {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 180px;
+  padding: 15px 5px;
+  color: #333;
+  background-color: #fff;
+  box-sizing: border-box;
+  margin-bottom: 10px;
+  .top {
+    flex: 1;
+    padding: 0 15px;
+    font-size: 14px;
+    height: 50px;
+    line-height: 50px;
+    img {
+      float: left;
+      width: 40px;
+      height: 40px;
+      margin-top: 5px;
+      margin-right: 10px;
+      border-radius: 50%;
+    }
+    .name {
+      position: relative;
+      float: left;
+      max-width: 50%;
+      color: #333;
+      font-size: 16px;
+      font-weight: bold;
+      @include ellipsis(1);
+      // &:after {
+      //   position: absolute;
+      //   top: 20px;
+      //   right: 75px;
+      //   content: '';
+      //   width: 8px;
+      //   height: 8px;
+      //   border: 1px solid #999;
+      //   border-width: 1px 1px 0 0;
+      //   transform: rotate(45deg);
+      // }
+    }
+    .status {
+      float: right;
+      color: #999;
+    }
+  }
+  .mid {
+    flex: 2;
+    margin-left: 62px;
+    margin-right: 15px;
+    padding-top: 10px;
+    box-sizing: border-box;
+    font-size: 14px;
+    color: #666;
+    @include onepx('top');
+    .food {
+      line-height: 25px;
+      font-size: 12px;
+      span {
+        font-size: 14px;
+        color: #151515;
+        float: right;
+      }
+    }
+    .cost {
+      float: right;
+      font-size: 12px;
+      line-height: 25px;
+      span {
+        font-size: 14px;
+        color: #151515;
+      }
+    }
+  }
+  .bottom {
+    flex: 1;
+    @include onepx('top');
+    padding: 0 15px;
+    font-size: 14px;
+    height: 24px;
+    line-height: 24px;
+    .again {
+      float: right;
+      width: 76px;
+      height: 24px;
+      line-height: 24px;
+      border: 1px solid #ccc;
+      text-align: center;
+      font-size: 14px;
+      border-radius: 2px;
+      margin: 9px 0;
+    }
+  }
+}
+</style>
---
+++ src/views/WishList.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="wishlist">
+    <!-- <div v-for="item in wishlistData" :key="item.name" >
+      <wishlist-item :data="item"></wishlist-item>
+    </div> -->
+
+    <WishListItem v-for="item in wishlistData"
+                    :key="item.name"
+                    :data="item"
+                    @toRestaurant="toRestaurant()"></WishListItem>
+
+    <tab-bar></tab-bar>
+  </div>
+</template>
+
+<script>
+import TabBar from '@/components/tab-bar/tab-bar'
+import WishListItem from '@/components/wishlist-item'
+import IndexList from '@/mock/index-list.json'
+// import axios from 'axios'
+
+export default {
+  components: {
+    TabBar,
+    WishListItem
+  },
+  data () {
+    return {
+      wishlistData: IndexList.data.poilist
+    }
+  },
+  props: {},
+  watch: {},
+  methods: {
+      toRestaurant () {
+      this.$router.push({
+        path: '/restaurant'
+      })
+     },
+    // 初始化列表数据
+//     _initIndexListData () {
+//       axios.get('/api/indexList').then(res => {
+//         // console.log(res)
+//         if (res.data.code === 0) {
+//           this.orderData = res.data.data.data.poilist
+//         }
+//       }).catch(err => {
+//         console.log(err)
+//       })
+//     }
+   },
+  filters: {},
+  computed: {},
+  created () {
+    // 初始化列表数据
+    // this._initIndexListData()
+  },
+  mounted () {},
+  destroyed () {}
+}
+</script>
