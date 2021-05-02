2a8b5b98eec549657487df01649b72739a69bea9
chenhu2015 <chenhu2015@users.noreply.github.com>
2021-05-02 09:53:56 -0500

    some changes

--- src/views/WishList.vue
+++ src/views/WishList.vue
@@ -1,47 +1,58 @@
 <template>
-  <div class="wishlist-item" >
-    <div class="top" >
-      <div>{{restaurant.restaurant_id}}</div>
-      <img :src="restaurant.restaurant_avatar" @click="toRestaurant(data)">
-      <!-- v-lazy -->
-      <div class="name" @click="toRestaurant(data)">{{restaurant.restaurant_name}}</div>
-      
-      <!-- <div class="status">5 orders complete</div> -->
-    </div>
-
-    <!-- <div class="mid">
-      <div class="food">Average price: {{data.average_price_tip}}<span></span></div>
-      <div class="food">Average deliverry time: {{data.avg_delivery_time}}<span></span></div>
-      <div class="food">  </div>
-      <div class="cost"><span></span></div>
+  <div class="wishlist">
+    <!-- <div v-for="item in wishlist" :key="item.name" >
+      <wishlist-item :data="item"></wishlist-item>
     </div> -->
-
-    <div class="bottom">
-      <span class="again">Remove</span>
-      <span class="again" @click="commentPage">Comment</span>
-      
-      
-    </div>
+    <div>{{user.uid}}</div>
+    <top-bar></top-bar>
+    <b-container>
+      <b-row>
+    <WishListItem v-for="item in wishlistData"
+                    :key="item.name"
+                    :data="item"
+                    @toRestaurant="toRestaurant()"></WishListItem>
+    </b-row>
+      </b-container>
+    <tab-bar></tab-bar>
   </div>
 </template>
 
 <script>
+import TabBar from '@/components/tab-bar/tab-bar'
+import WishListItem from '@/components/wishlist-item'
+import IndexList from '@/mock/index-list.json'
+// import TitleBar from '@/components/title-bar'
+import TopBar from '@/components/top-bar'
+// import axios from 'axios'
+
 import {db} from "../firebaseConfig.js"
+
 import { auth, provider } from "@/firebaseConfig";
+
 export default {
-  components: {},
+  components: {
+    TabBar,
+    // TitleBar,
+    WishListItem,
+    TopBar
+  },
   data () {
     return {
-      wishlist:[],
-      restaurant:[],
-      user:[],
-      count: null,
-      
+      wishlistData: IndexList.data.poilist,
+      user: [],
+      wishlist: [],
     }
   },
-  props: ["id"],
-  
+  props: {},
   watch: {},
+
+  firestore: function() {
+        return {
+            wishlist: db.collection("wishlist").where("person_id","==",auth.currentUser.uid),
+            
+        }
+  },
+
   beforeCreate: function() {
     auth.onAuthStateChanged(user => {
       // console.log("user state:", user);
@@ -51,31 +62,17 @@ export default {
       }
     });
   },
+
+
+
   methods: {
-    
-    commentPage: function() {
-            
-            //var res
-            //db.collection("historylist").where("restaurant_id","==",this.restaurant.restaurant_id).get().then((res) => console.log(res.size))
-            //db.collection('historylist').get().then(snapshot => console.log(snapshot.size))
-            //console.log(db.collection('historylist').get().then(snapshot => {return snapshot.size}))
-            //db.collection('historylist').get().then(snapshot => {
-            //  this.count=snapshot.size;
-            //  })
-            //console.log(this.count)
-            
-            //if ( count==0){
-              const newRestaurant = {comments:"", person_avatar:this.user.photoURL, person_id:this.user.uid, restaurant_avatar:this.restaurant.restaurant_avatar, restaurant_id:this.restaurant.restaurant_id, restaurant_name:this.restaurant.restaurant_name, restaurant_snapshot:""};
-              db.collection("historylist").add(newRestaurant)
-            //}
-            this.$router.push({ name: 'Comment',
-            params: {
-              rid:this.restaurant.restaurant_id
-            }
-            })
-        },
-    
-    signInWithGoogle: function() {
+      toRestaurant () {
+      this.$router.push({
+        path: '/restaurant'
+      })
+     },
+
+     signInWithGoogle: function() {
       auth.signInWithRedirect(provider)
         .then(result => {
           this.user = result.user;
@@ -88,125 +85,39 @@ export default {
           this.user = null;
         })
         .catch(err => console.log(err));
-    },
-       toRestaurant (data) {
-      // console.log(data)
-      this.$emit('toRestaurant', data)
-      
-      
     }
-  },
+
+    // 初始化列表数据
+//     _initIndexListData () {
+//       axios.get('/api/indexList').then(res => {
+//         // console.log(res)
+//         if (res.data.code === 0) {
+//           this.orderData = res.data.data.data.poilist
+//         }
+//       }).catch(err => {
+//         console.log(err)
+//       })
+//     }
+   },
   filters: {},
   computed: {},
-  created () {},
-  firestore: function() {
-        return {
-            restaurant: db.collection("wishlist").doc(this.id),
-            
-        }
+  created () {
+    // 初始化列表数据
+    // this._initIndexListData()
   },
-  mounted () {}
+  mounted () {},
+  destroyed () {}
 }
 </script>
 
 <style lang="scss" scoped>
 @import '~@/assets/scss/const.scss';
 @import '~@/assets/scss/mixin.scss';
-.wishlist-item {
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  height: 180px;
-  padding: 15px 5px;
-  color: #333;
-  background-color: #fff;
-  box-sizing: border-box;
-  margin-bottom: 10px;
-  .top {
-    flex: 1;
-    padding: 0 15px;
-    font-size: 14px;
-    height: 50px;
-    line-height: 50px;
-    img {
-      float: left;
-      width: 40px;
-      height: 40px;
-      margin-top: 5px;
-      margin-right: 10px;
-      border-radius: 50%;
-    }
-    .name {
-      position: relative;
-      float: left;
-      max-width: 50%;
-      color: #333;
-      font-size: 16px;
-      font-weight: bold;
-      @include ellipsis(1);
-      // &:after {
-      //   position: absolute;
-      //   top: 20px;
-      //   right: 75px;
-      //   content: '';
-      //   width: 8px;
-      //   height: 8px;
-      //   border: 1px solid #999;
-      //   border-width: 1px 1px 0 0;
-      //   transform: rotate(45deg);
-      // }
-    }
-    .status {
-      float: right;
-      color: #999;
-    }
-  }
-  .mid {
-    flex: 2;
-    margin-left: 62px;
-    margin-right: 15px;
-    padding-top: 10px;
-    box-sizing: border-box;
-    font-size: 14px;
-    color: #666;
-    @include onepx('top');
-    .food {
-      line-height: 25px;
-      font-size: 12px;
-      span {
-        font-size: 14px;
-        color: #151515;
-        float: right;
-      }
-    }
-    .cost {
-      float: right;
-      font-size: 12px;
-      line-height: 25px;
-      span {
-        font-size: 14px;
-        color: #151515;
-      }
-    }
-  }
-  .bottom {
-    flex: 1;
-    @include onepx('top');
-    padding: 0 15px;
-    font-size: 14px;
-    height: 24px;
-    line-height: 24px;
-    .again {
-      float: right;
-      width: 76px;
-      height: 24px;
-      line-height: 24px;
-      border: 1px solid #ccc;
-      text-align: center;
-      font-size: 14px;
-      border-radius: 2px;
-      margin: 9px 0;
-    }
-  }
+
+
+.topbar{
+  height: 70px;
+  background-color: rgb(219, 207, 38);
 }
+
 </style>
